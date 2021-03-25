888bf40a5909d344c6c4688633cc11373c871478
Jonathan Cheng <7626923-jchengdev@users.noreply.gitlab.com>
2021-03-25 13:14:07 -0300

    fix: Gulp syntax update

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,36 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', ['static'], () => {
-  const tsResult = tsProject.src()
-    .pipe(tsProject());
-
-  return tsResult.js
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('static', ['clean'], () => {
-  return gulp
-    .src(['src/**/*.json', 'src/**/*.html'])
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('clean', () => {
-  return gulp
-    .src('dist')
-    .pipe(clean());
+  return gulp.src('dist', { allowEmpty: true }).pipe(clean());
 });
 
-gulp.task('build', ['clean', 'static', 'scripts']);
+gulp.task(
+  'static',
+  gulp.series('clean', () => {
+    return gulp.src(['src/**/*.json', 'src/**/*.html']).pipe(gulp.dest('dist'));
+  })
+);
 
-gulp.task('watch', ['build'], () => {
-  return gulp.watch(['src/**/*.ts', 'src/**/*.json', 'src/**/*.html'], ['build']);
-});
+gulp.task(
+  'scripts',
+  gulp.series('static', () => {
+    const tsResult = tsProject.src().pipe(tsProject());
 
-gulp.task('default', ['watch']);
+    return tsResult.js.pipe(gulp.dest('dist'));
+  })
+);
+
+gulp.task('build', gulp.series('clean', 'static', 'scripts'));
+
+gulp.task(
+  'watch',
+  gulp.series('build', () => {
+    return gulp.watch(
+      ['src/**/*.ts', 'src/**/*.json', 'src/**/*.html'],
+      gulp.series('build')
+    );
+  })
+);
+
+gulp.task('default', gulp.series('watch'));
